b89ce8fe3cdc5ccfb3576405a7b0eab87cdf39e1
Денис <ogut.mox.98@inbox.ru>
2025-10-29 15:30:55 +0300

    added dock widgets

--- popups/dashboard.py
+++ popups/dashboard.py
@@ -1,6 +1,7 @@
 from re import S
 import PySide6
-from PySide6.QtGui import QIcon, QPalette, QColor
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QIcon, QPalette, QColor
 from PySide6.QtWidgets import QPushButton, QHBoxLayout, QWidget, QMainWindow, QToolBar
 from custom_functions.load_css import LoadCss # используем мое самописное решение для стилей
 from popups.directory import DirectoryWindow
@@ -14,21 +15,28 @@ class Dashboard(QMainWindow):
         self.setWindowIcon(QIcon('images/logo.png'))
         self.showMaximized() # максиммально растягиваем на весь экран, но не фул скрин
         self.setWindowTitle('Dashboard')
+
         menu_bar = self.menuBar() # создаем объект класса MenuBar
 
         file_menu = menu_bar.addMenu('Проект') # Пользуемся функцией addMenu из menuBar
         file_menu.addAction('Сохранить') # Создаем опции
         file_menu.addAction('Выйти', lambda: self.close())
         
+        settings_menu = menu_bar.addMenu('Настройки')
+        settings_menu.addAction('Горячие клавиши')
+
+        
         toolbar = QToolBar('Main toolbar') # создаем тулбар
         self.addToolBar(toolbar) # добавляем объект класса QToolBar в само приложение
 
-        create_dir_button = QPushButton('Создать справочник')
-        create_dir_button.setToolTip('Создаст новый справочник') # при наведении больше чем на секунду, вылазит подсказка с этим текстом
-        toolbar.addWidget(create_dir_button)
-        create_dir_button.clicked.connect(self.create_directory) # функция открывающая окно справочника
+        create_dir_action = QAction('Создать справочник', self) # action это как кнопка только круче, можно добавлять картинки и шорткаты
+        create_dir_action.setShortcut('Ctrl+N') # создаем шорткат для кнопки
+        create_dir_action.triggered.connect(self.create_directory)
+
+        create_dir_action.setToolTip('Создаст новый справочник (Ctrl + N)') # при наведении больше чем на секунду, вылазит подсказка с этим текстом
+        toolbar.addAction(create_dir_action)
         toolbar.addSeparator()
 
     def create_directory(self):
-        self.dir = DirectoryWindow() # сохраняем как переменную всего класса чтобы окно не закрывалось из за сборщика мусора
-        self.dir.show()
+        dock = DirectoryWindow() # создаем объект класса DirectoryWindow
+        self.addDockWidget(Qt.LeftDockWidgetArea, dock) # добавляем объект класса DirectoryWindow в само приложение
--- popups/directory.py
+++ popups/directory.py
@@ -1,19 +1,14 @@
 from PySide6.QtGui import QPalette, QColor, QIcon
-from PySide6.QtWidgets import QMainWindow, QPushButton, QHBoxLayout, QWidget
+from PySide6.QtWidgets import QDockWidget, QPushButton, QHBoxLayout, QWidget
 from custom_functions.load_css import LoadCss
-class DirectoryWindow(QMainWindow):
+class DirectoryWindow(QDockWidget): # QDockWidget это рамка + механика состыковки, потому надо внутри добавить QWidget
     def __init__(self):
         super().__init__()
         self.setStyleSheet(LoadCss().load_file('styles/dashboard_styles.css')) # используем мою самописную гениальную технологию
         self.setWindowTitle('Новый справочник')
-        self.setGeometry(0, 0, 1600, 900)
         self.setWindowIcon(QIcon('images/logo.png')) # ставим иконки
-        menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu('&Файл') # создаем меню (которое сверху во всех прогах)
-        file_menu.addAction('Загрузить из CSV')
-        file_menu.addAction('Сохранить') # добавляем опции внутри этого меню
-        quit_action = file_menu.addAction('Выйти') # добавляем опции внутри этого меню
-        quit_action.triggered.connect(self.close)
-        edit_menu = menu_bar.addMenu('Редактировать')
-        edit_menu.addAction('Шаг назад')
-        edit_menu.addAction('Шаг вперед')
+        # QDockWidget это обертка для моего контейнера
+        container = QWidget() # создаем контейнер для кнопки
+        container.setLayout(QHBoxLayout()) # создаем макет для кнопки
+        container.layout().addWidget(QPushButton('ТЕСТ')) # добавляем кнопку в макет
+        self.setWidget(container) # Не addWidget, в QDockWidget нет addWidget, только setWidget, единственный виджет который можно добавить
