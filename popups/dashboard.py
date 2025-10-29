6f46949977d38062eddf347695befdc9c4d2b0e4
Денис <ogut.mox.98@inbox.ru>
2025-10-29 14:32:01 +0300

    added some features

--- popups/dashboard.py
+++ popups/dashboard.py
@@ -1,4 +1,5 @@
 from re import S
+import PySide6
 from PySide6.QtGui import QIcon, QPalette, QColor
 from PySide6.QtWidgets import QPushButton, QHBoxLayout, QWidget, QMainWindow, QToolBar
 from custom_functions.load_css import LoadCss # используем мое самописное решение для стилей
@@ -12,7 +13,7 @@ class Dashboard(QMainWindow):
         self.setStyleSheet(LoadCss().load_file('styles/dashboard_styles.css')) # используем мою самописную гениальную технологию
         self.setWindowIcon(QIcon('images/logo.png'))
         self.showMaximized() # максиммально растягиваем на весь экран, но не фул скрин
-
+        self.setWindowTitle('Dashboard')
         menu_bar = self.menuBar() # создаем объект класса MenuBar
 
         file_menu = menu_bar.addMenu('Проект') # Пользуемся функцией addMenu из menuBar
@@ -21,10 +22,13 @@ class Dashboard(QMainWindow):
         
         toolbar = QToolBar('Main toolbar') # создаем тулбар
         self.addToolBar(toolbar) # добавляем объект класса QToolBar в само приложение
+
         create_dir_button = QPushButton('Создать справочник')
+        create_dir_button.setToolTip('Создаст новый справочник') # при наведении больше чем на секунду, вылазит подсказка с этим текстом
         toolbar.addWidget(create_dir_button)
         create_dir_button.clicked.connect(self.create_directory) # функция открывающая окно справочника
         toolbar.addSeparator()
+
     def create_directory(self):
         self.dir = DirectoryWindow() # сохраняем как переменную всего класса чтобы окно не закрывалось из за сборщика мусора
         self.dir.show()
